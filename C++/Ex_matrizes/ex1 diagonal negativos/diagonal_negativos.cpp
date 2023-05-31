f02dd70e1e34d39bd6418a37ec0598bcf9508bf0
Allanpt <jatallantavares@gmail.com>
2023-05-31 10:47:36 -0300

    Exercícios em C++

---
+++ C++/Ex_matrizes/ex1 diagonal negativos/diagonal_negativos.cpp
@@ -0,0 +1,64 @@
+/*
+Exemplo: 
+Qual a ordem da matriz? 3
+Elemento [0,0]: 5 
+Elemento [0,1]: -3 
+Elemento [0,2]: 10
+Elemento [1,0]: 15 
+Elemento [1,1]: 8 
+Elemento [1,2]: 2
+Elemento [2,0]: 7 
+Elemento [2,1]: 9 
+Elemento [2,2]: -4
+DIAGONAL PRINCIPAL: 
+5 8 -4 
+QUANTIDADE DE NEGATIVOS = 2 
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{ 
+    int n;
+
+    cout << "Qual a ordem da matriz? ";
+    cin >> n;
+
+    int mat[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "Elemento [" << i << "," << j << "]: ";
+            cin >> mat[i][j];
+        }
+    }
+
+    cout << "DIAGONAL PRINCIPAL:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << mat[i][i] << " ";
+        
+    }
+    cout << endl;
+
+    int soma_negativos = 0;
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (mat[i][j] < 0)
+            {
+                soma_negativos++;
+            }
+            
+        }
+        
+    }
+    
+    cout << "QUANTIDADE DE NEGATIVOS = " << soma_negativos << endl;
+    return 0;
+}
